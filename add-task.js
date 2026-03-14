43c7f3f060eab16cbe7d8b0f4cdcdf85a86a3ed5
marccharter-cpu <marc.charter@gmail.com>
2026-03-14 13:59:19 -0500

    Update add-task.js

--- add-task.js
+++ add-task.js
@@ -1,1 +1,12 @@
 
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Only POST allowed" });
+  }
+
+  const { text } = req.body;
+
+  console.log("Received text:", text);
+
+  return res.status(200).json({ received: text });
+}
